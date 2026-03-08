1cb2f884e52313dac440a578160fd8609b652923
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-03-08 13:20:38 +0530

    Feat: Solved LC-705. Manual HashSet implementation using modulo hashing and buckets

---
+++ 705-design-hashset.py
@@ -0,0 +1,18 @@
+class MyHashSet:
+
+    def __init__(self):
+       self.size=1000 
+       self.table=[[] for i in range(self.size)]
+    def add(self, key: int) -> None:
+        index = key % self.size
+        if key not in self.table[index]:
+            self.table[index].append(key)
+
+    def remove(self, key: int) -> None:
+        index = key % self.size
+        if key in self.table[index]:
+            self.table[index].remove(key)
+
+    def contains(self, key: int) -> bool:
+        index =key % self.size
+        return key in self.table[index]
