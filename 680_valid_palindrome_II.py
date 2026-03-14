d573b56606259593f18b49f8f2eae049646dbda9
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-03-14 13:52:20 +0530

    Feat: Solved LC-680. Valid Palindrome II

---
+++ 680_valid_palindrome_II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left=0
+        right=len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
+                return self.checkPalindrome(s, left+1,right) or self.checkPalindrome(s, left, right-1)
+            left+=1
+            right-=1
+        return True
+    
+    def checkPalindrome(self, s: str, left: int, right: int) -> bool:
+        while left<right:
+            if s[left]!=s[right]:
+                return False
+            left+=1
+            right-=1
+        return True
