8ec6d64d1f22fa2b036eb3b0d513b662a0d76904
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-03-17 12:12:25 +0530

    Feat: Solved LC-18. 4Sum using nested pivots and two-pointer squeeze

---
+++ 018_4sum.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        container=[]
+        for i in range(len(nums)):
+            if i > 0  and  nums[i]==nums[i-1]:
+                continue
+            
+            for j in range(i+1, len(nums)):
+                if j >i+1 and nums[j]==nums[j-1]:
+                    continue
+
+                left = j+1
+                right = len(nums)-1
+
+                while left < right:
+                    current_sum= nums[i] + nums[j]+ nums[left] + nums[right]
+
+                    if current_sum ==target:
+                        container.append([nums[i], nums[j], nums[left], nums[right]]) 
+
+                        left+=1
+
+                        while nums[left]==nums[left-1] and left<right:
+                            left+=1
+                    elif current_sum > target:
+                        right-=1
+                    else:
+                        left+=1
+        return container
