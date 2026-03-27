9597682cbe85a1c0dbafad7bb91917e5396e1fc8
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-03-27 11:10:44 +0530

    Feat : Solved LC - 150. Evaluate reverse polish notation using stack

---
+++ 150_Evaluate_Reverse_Polish_Notation.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack=[]
+        for i in tokens:
+            if i not in '+-/*':
+                stack.append(int(i))
+            else:
+                b=stack.pop()
+                a=stack.pop()
+                if i == "+":
+                    stack.append(a+b)
+                if i == "-":
+                    stack.append(a-b)
+                if i == "/":
+                    stack.append(int(a/b))
+                if i == "*":
+                    stack.append(a*b)
+        return stack[0]
