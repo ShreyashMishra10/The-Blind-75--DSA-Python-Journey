38b70f55db47f2c4941b39d2c29c43b7f5c21360
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-03-28 13:01:50 +0530

    Feat: Solved LC - 901. Online stock span

---
+++ 901_Online_Stock_Span.py
@@ -0,0 +1,16 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack=[]
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0]<=price:
+            price_prev, span_prev= self.stack.pop()
+            span+=span_prev
+        self.stack.append([price, span])
+        return span
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
