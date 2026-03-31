babc2bdc988ccfd2aa8aa893aed2640e8efce882
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-03-31 12:59:18 +0530

    Feat: Solved LC- 74. Search a 2D matrix

---
+++ 074_Search_a_2D_Matrix.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row=len(matrix)
+        col = len(matrix[0])
+        left=0
+        right=(row * col)-1
+        while left <=right:
+            mid = (left + right )//2
+            val = matrix[mid // col][mid % col]
+            if val == target:
+                return True
+            elif val >target:
+                right = mid -1
+            else:
+                left = mid + 1
+        return False
